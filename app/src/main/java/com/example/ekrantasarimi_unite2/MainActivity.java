51e5c352ed1c4415de1a9cddbafb1807cbd520ba
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-10-28 12:09:31 +0300

    no message

--- app/src/main/java/com/example/ekrantasarimi_unite2/MainActivity.java
+++ app/src/main/java/com/example/ekrantasarimi_unite2/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.ekrantasarimi_unite2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnUyg3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnUyg3 = findViewById(R.id.btnUyg3);
+
+        btnUyg3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent uyg3 = new Intent(MainActivity.this, Uyg3Activity.class);
+                startActivity(uyg3);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ekrantasarimi_unite2/Uyg3Activity.java
@@ -0,0 +1,30 @@
+package com.example.ekrantasarimi_unite2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Uyg3Activity extends AppCompatActivity {
+
+    Button btnGeri;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.uyg3_activity);
+
+        btnGeri = findViewById(R.id.btnGeri);
+
+        btnGeri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent geri = new Intent(Uyg3Activity.this, MainActivity.class );
+                startActivity(geri);
+            }
+        });
+    }
+}
